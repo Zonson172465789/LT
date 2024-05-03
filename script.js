aa5c6a99af7f656f335edaa23056472dc7eed65b
Zonson172465789 <168737494+Zonson172465789@users.noreply.github.com>
2024-05-03 01:11:12 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+    fetch('https://docs.google.com/spreadsheets/d/1BjdhXuDmgQvNo0OvXrIONtQz0Ast0-Nz2o2asBuIj5c/edit?usp=sharing')
+        .then(response => response.json())
+        .then(data => {
+            const contentDiv = document.getElementById('content');
+            const randomIndex = Math.floor(Math.random() * data.length);
+            const title = data[randomIndex].title;
+            const links = data[randomIndex].links;
+            contentDiv.innerHTML = `<h1>${title}</h1>`;
+            const ul = document.createElement('ul');
+            links.forEach(link => {
+                const li = document.createElement('li');
+                li.innerHTML = `<a href="${link}">${link}</a>`;
+                ul.appendChild(li);
+            });
+            contentDiv.appendChild(ul);
+        })
+        .catch(error => console.error('Error:', error));
+};
